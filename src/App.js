07a179428aaaa0bebbdf41783c02aaf82b60384d
chgim <chgim1128@gmail.com>
2022-11-21 23:36:33 +0900

    up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Home from './pages/Home';
+import Diary from './pages/Diary';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import RouteTest from './components/RouteTest'
 function App() {
   return (
+   <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>app.js</h2>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/new" element={<New />}/>
+        <Route path="/edit" element={<Edit />}/>
+        <Route path="/diary/:id" element={<Diary />}/> {/*diary에는 아이디가 없는 일기가 없음.*/}
+      </Routes>
+      <RouteTest />
     </div>
+    </BrowserRouter>
   );
 }
 
