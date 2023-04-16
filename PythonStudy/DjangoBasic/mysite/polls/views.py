434c340becbcba91d862a03e091601cfe34a855b
chgim <chgim1128@gmail.com>
2023-04-16 17:47:42 +0900

    update

--- PythonStudy/DjangoBasic/mysite/polls/views.py
+++ PythonStudy/DjangoBasic/mysite/polls/views.py
@@ -1,22 +1,40 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 
-from .models import Question
+from django.urls import reverse
+from .models import Choice, Question
 
 # Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
     # 쿼리 실행 안되어있음.
-    output = ", ".join([q.question_text for q in latest_question_list])
+    context={'latest_question_list':latest_question_list,}
+    return render(request, "index.html",context)
     # 쿼리를 실행하고 문자열 조인.
-    return HttpResponse(output)
+    
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    try:
+       question=Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:# 특정 예외에 대해 catch 하는것을 권장
+        raise Http404("Question does not exist.")
+    return render(request, "detail.html", {"question":question}) # 파이썬은 예외 발생 시 그대로 프로그램 꺼지기 때문에 예외처리
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at results of question {question_id}")
+    question=get_object_or_404(Question, pk=question_id)
+    return render(request, "results.html", {"question":question})
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    question=get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "detail.html",{
+            "question":question,
+            "error_message":"선택을 안했어 임마",}
+        )
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+        return HttpResponseRedirect(f"/polls/{question_id}/results/")  
--- PythonStudy/Openpyxl/3st-1.py
+++ PythonStudy/Openpyxl/3st-1.py
@@ -1,3 +1,4 @@
+# 문제
 # 3 6 9 게임
 
 # 1. 컴퓨터부터 입력을 시작합니다.
@@ -10,32 +11,37 @@
 
 # 4. 잘못된 값을 입력하면 "컴퓨터 승리" 또는 "사용자 승리" 출력합니다.
 
-import random 
+# 정답
+import random # random 함수를 사용하기 위해 random 모듈 임포트 해오기
 
-count=0
-while True:
-    count+=1
-    if '3' in str(count) or '6' in str(count) or '9' in str(count):
-       answer=" "  * (str(count).count('3')+str(count).count('6')+str(count).count('9'))
-    else:
-        answer=count
+count=0 # 첫 카운트 시작은 0으로 시작.
 
-    if random.randint(1,10)==1:
-        print("사용자 승리")
-        break
-    else:
-        print(f'컴퓨터:{answer}')    
+while True: # while문 무한 반복 ( 특정 조건을 충족 시켜야 나갈 수 있음 ex) break )
 
+    # 컴퓨터 차례
+    count+=1 # count 변수 값 1 증가 
+    if '3' in str(count) or '6' in str(count) or '9' in str(count): # 만약 count 값에 '3', '6', '9' 중 하나의 문자라도 있다면 (문자열과 비교하기 위해 str(count)로 변환)  
+       answer=" "  * (str(count).count('3')+str(count).count('6')+str(count).count('9')) # count에서 '3','6','9'의 개수만큼 answer에 " "(띄어쓰기) 대입. ex)33->" "" " 
+    else: # 아닐 경우
+        answer=count # answer에 count 값 대입
+
+    if random.randint(1,100)==1: # 1부터 100 사이의 임의의 정수 값이 1이 나올 경우 (1% 확률로)
+        print("사용자 승리") # 사용자 승리 출력
+        break # while문 나가기 (프로그램 종료)
+    else: # 아닐 경우
+        print(f'컴퓨터:{answer}') # 컴퓨터의 answer 값 가져와서 출력   
+
+    # 사용자 차례
+    count+=1 # 카운트 변수 값 1 증가
+    if '3' in str(count) or '6' in str(count) or '9' in str(count): # 만약 count 값에 '3', '6', '9' 중 하나의 문자라도 있다면 (문자열과 비교하기 위해 str(count)로 변환)  
+       answer=" "  * (str(count).count('3')+str(count).count('6')+str(count).count('9'))  # count에서 '3','6','9'의 개수만큼 answer에 " "(띄어쓰기) 대입. ex)33->" "" " 
+    else: # 아닐 경우
+        answer=str(count) # answer에 str로 변환한 count 값 대입(사용자의 입력은 문자열로 받기 때문)
     
+    human=input("사용자 입력:") # 사용자 입력 받고 human 변수에 저장.(문자열 형태임)
 
-    count+=1
-    if '3' in str(count) or '6' in str(count) or '9' in str(count):
-       answer=" "  * (str(count).count('3')+str(count).count('6')+str(count).count('9'))
-    else:
-        answer=str(count)
-    human=input("사용자 입력:")
-    if human!=answer:
-        print("컴퓨터 승리")
-        break
-    else:
-        pass
+    if human!=answer: # 만약 human 과 answer의 값이 일치하지 않는다면(사용자 입력과 answer 값이 불일치)
+        print("컴퓨터 승리") # 컴퓨터 승리 출력
+        break  # while문 나가기 (프로그램 종료)
+    else: # 아닐 경우
+        pass # 넘어간다(다음 컴퓨터 차례)
